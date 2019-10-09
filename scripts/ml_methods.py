92ad390700e1c9d27814cc1f76226db14a7e8e59
Anel <muhamedagic.anel@gmail.com>
2019-10-09 17:56:33 +0200

    first commit

---
+++ scripts/ml_methods.py
@@ -0,0 +1,148 @@
+# Useful starting lines
+import numpy as np
+import matplotlib.pyplot as plt
+import importlib
+
+from proj1_helpers import *
+
+#initial_w is the initial weight vector
+#gamma is the step-size
+#max_iters is the number of steps to run
+#lambda_ is always the regularization parameter
+
+def least_squares_GD(y, tx, initial_w, max_itters, gamma):
+    print("calc Linear regression using gradient descent")
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    w = initial_w
+    for n_iter in range(max_itters):
+        # ***************************************************
+        # compute gradient and loss
+        # ***************************************************
+        loss = compute_loss_mse(y, tx, w)
+        gradient = least_squares(y,tx)
+        print(gradient)
+       # ***************************************************
+        # update w by gradient
+        # ***************************************************
+        w = w + gamma*gradient
+        print("Gradient Descent({bi}/{ti}): loss={l},w={w}".format(
+              bi=n_iter, ti=max_itters - 1, l=loss, w=w))
+
+    return loss, w
+
+
+def linear_regresssion_GD_mse(y, tx, initial_w, max_itters, gamma):
+    print("calc Linear regression using gradient descent")
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    losses = []
+    w = initial_w
+    for n_iter in range(max_itters):
+        # ***************************************************
+        # compute gradient and loss
+        # ***************************************************
+        loss = compute_loss_mse(y, tx, w)
+        gradient = compute_gradient_mse(y, tx, w)
+        print(gradient)
+       # ***************************************************
+        # update w by gradient
+        # ***************************************************
+        w = w + gamma*gradient
+
+        print("Gradient Descent({bi}/{ti}): loss={l},w={w}".format(
+              bi=n_iter, ti=max_itters - 1, l=loss, w=w))
+
+    return losses, w
+
+def least_squares_SGD(y,tx,initial_w,max_iters,gamma):
+    print("Calc Linear regression using stochastic gradient descent")
+    """Stochastic gradient descent algorithm."""
+    # ***************************************************
+    # implement stochastic gradient descent.
+    # ***************************************************
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        print("MINIBATCHES COMING")
+        print(minibatch_y)
+        print(minibatch_tx)
+        # Define parameters to store w and loss
+        ws = [initial_w]
+        w = initial_w
+        for n_iter in range(max_iters):
+            # ***************************************************
+            # compute gradient and loss
+            # ***************************************************
+            loss = compute_loss_mse(minibatch_y,minibatch_tx,w)
+            gradient=compute_gradient_mse(minibatch_y,minibatch_tx,w)
+            print("loss = " + str(loss))
+            print("gradient = " + str(gradient))
+            # ***************************************************
+            # update w by gradient
+            # ***************************************************
+            w = w+np.multiply(gamma,gradient)
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+            print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+                  bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return loss, ws
+
+def least_squares(y,tx):
+    print("Calc Least squares regression using normal equations")
+    return np.linalg.inv(np.transpose(tx)@tx)@np.transpose(tx)@y
+
+def ridge_regression(y,tx,lambda_):
+    print("Ridge regression using normal equations")
+    """implement ridge regression."""
+    # ***************************************************
+    # ridge regression: TODO
+    # ***************************************************
+    w = np.linalg.inv(np.transpose(tx) @ tx + lambda_) @ np.transpose(tx) @ y
+    # w = np.linalg.inv(np.transpose(tx)@tx+np.identity(lambda_))@np.transpose(tx)@y
+    summe = np.sum(np.power(y - tx @ w, 2))
+    summe = summe / (2 * len(y))
+    result = summe + lambda_ * np.sum(w ** 2)
+    return result
+
+def logistic_regression(y,tx,initial_w, max_itters,gamma):
+    print("Logistic regression using gradient descent or SGD")
+    # "Logistic regression is the appropriate regression analysis to conduct when theession produces a logistic curve, which is limited to values between 0 and 1. Logistic regression is similar to a linear regression, but the curve is constructed using the natural logarithm of the “odds” of the target variable, rather than the probab dependent variable is dichotomous (binary)." - from the Internet
+    # https://towardsdatascience.com/building-a-logistic-regression-in-python-301d27367c24
+
+
+    sigmoid = 1/(1+np.exp(-tx))
+
+    w = initial_w
+
+    for i in range(max_itters):
+        pediction = np.dot(w*tx)
+        activationFunction = sigmoid(pediction) # make between 0-1
+        print(activationFunction)
+
+
+
+def reg_logisitc_regression(y,tx,lambda_,initial_w,max_iters,gamma):
+    print("Regularized logistic regression using gradient descent or SGD")
+
+
+DATA_TRAIN_PATH = '../data/train.csv'
+y, tx, ids = load_csv_data(DATA_TRAIN_PATH)
+w = np.zeros(30)
+
+#losses, w_updated_mse = linear_regresssion_GD_mse(y, tx, w, 100, 0.2)
+losses, w_updated_leastSquares = least_squares_GD(y, tx, w, 100, 0.2)
+
+#print(w_updated_mse)
+print(w_updated_leastSquares)
+
+w_compare = [-2.80368247e+01,5.39471822e-01,-1.00522284e+01,1.13621369e+01,4.25777258e+02,5.05303694e+02,4.25229617e+02,-4.92012404e-01,3.50325763e+00,3.41860458e+01,2.13402302e-02,9.88750534e-02,4.25298113e+02,-5.73547481e+00,7.63626536e-03,1.38542432e-02,-1.44032246e+00,1.02196190e-02,-1.20750185e-02,3.44476629e+00,-3.26639200e-03,2.92616549e+01,5.18460080e-01,2.19436118e+02,2.03730018e+02,2.03732874e+02,4.43313020e+02,4.25186123e+02,4.25193362e+02,4.13618429e+01]
+
+
+DATA_TEST_PATH = '../data/test.csv'
+y_test, tx_test, ids_test = load_csv_data(DATA_TEST_PATH)
+
+
+y_pred = predict_labels(w_updated_leastSquares, tx_test)
+
+create_csv_submission(ids_test,y_pred,"fyujn")
---
+++ scripts/proj1_helpers.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""some helper functions for project 1."""
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def load_csv_data(data_path, sub_sample=False):
+    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
+
+    # convert class labels from strings to binary (-1,1)
+    yb = np.ones(len(y))
+    yb[np.where(y=='b')] = -1
+    
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        input_data = input_data[::50]
+        ids = ids[::50]
+
+    return yb, input_data, ids
+
+
+def predict_labels(weights, data):
+    """Generates class predictions given weights, and a test data matrix"""
+    y_pred = np.dot(data, weights)
+    y_pred[np.where(y_pred <= 0)] = -1
+    y_pred[np.where(y_pred > 0)] = 1
+    
+    return y_pred
+
+
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+
+
+
+def sigm(tx):
+    return 1/(1+np.exp(-tx))
+
+def compute_loss_mse(y, tx, w): # via mse
+    """Calculate the loss.
+    You can calculate the loss using mse or mae.
+    """
+#    summe = np.sum(y - tx @ w)
+    ypred = predict_labels(w,tx)
+    summe = np.sum((y - ypred)**2)
+    mse = summe / 2*len(y)
+
+    return mse
+
+def split_data(x, y, ratio, seed=1):
+    """
+    split the dataset based on the split ratio. If ratio is 0.8
+    you will have 80% of your data set dedicated to training
+    and the rest dedicated to testing
+    """
+    # set seed
+    np.random.seed(seed)
+
+    x_training = x[:round(len(x)*ratio)]
+    x_testing = x[round(len(x)*ratio):]
+    y_training = y[:round(len(x)*ratio)]
+    y_testing = y[round(len(x)*ratio):]
+
+    return x_training, x_testing, y_training, y_testing
+
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    # ***************************************************
+    # polynomial basis function
+    # this function should return the matrix formed
+    # by applying the polynomial basis to the input data
+    # ***************************************************
+
+    result = np.zeros((len(x), degree + 1))
+    for i in range(len(x)):
+        for j in range(degree + 1):
+            result[i][j] = x[i] ** j
+
+    return result
+
+
+def compute_gradient_mse(y, tx, w):
+    error = y - predict_labels(w,tx)
+    gradient = -1/len(y)*np.dot(np.transpose(tx),error)
+
+    return gradient
+
+def compute_gradient_least_square(y, tx, w):
+    error = y - predict_labels(w,tx)
+    gradient = -1/len(y)*np.dot(np.transpose(tx),error)
+
+    return gradient
+
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+def generate_w(num_intervals):
+    """Generate a grid of values for w0 and w1."""
+    w0 = np.linspace(-100, 200, num_intervals)
+    w1 = np.linspace(-150, 150, num_intervals)
+    return w0, w1
+
+
+def get_best_parameters(w0, w1, losses):
+    """Get the best w from the result of grid search."""
+    min_row, min_col = np.unravel_index(np.argmin(losses), losses.shape)
+    return losses[min_row, min_col], w0[min_row], w1[min_col]
+
